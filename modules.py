07018f0a1f3821a47fe70368a44e76b782a8cbf7
Marcos Felipe <p.marcosfelipe@gmail.com>
2024-10-04 22:22:48 +0100

    installing and using external module for requests

--- modules.py
+++ modules.py
@@ -9,3 +9,10 @@ print(f"square: {square}")
 print("\nCreating and importing a new module")
 print(greetings("Marcos"))
 print(f"Double of 5 is {double(5)}")
+
+
+import requests
+url = "https://www.example.com"
+
+response = requests.get(url)
+print(f"\nHTTP request to {url} returned the status {response.status_code}")
